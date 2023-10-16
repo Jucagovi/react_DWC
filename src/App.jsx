c0eb742e272aace9e234f3f19496660f5059c920
Jucagovi <jc.gomez@edu.gva.es>
2023-10-16 18:17:47 +0200

    Práctica 2.08

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "./App.css";
-import Contenedor from "./componentes/UT02/posicionamiento/Contenedor.jsx";
+import Contenedor from "./componentes/UT02/practica2.08/Contenedor.jsx";
+import Cabecera from "./componentes/UT02/practica2.08/Cabecera.jsx";
+import Peliculas from "./componentes/UT02/practica2.08/Peliculas.jsx";
 import Contenido from "./componentes/UT02/posicionamiento/Contenido.jsx";
 import MasContenido from "./componentes/UT02/posicionamiento/MasContenido.jsx";
 import Pie from "./componentes/UT02/posicionamiento/Pie.jsx";
@@ -8,12 +10,19 @@ import Pie from "./componentes/UT02/posicionamiento/Pie.jsx";
 function App() {
   return (
     <React.Fragment>
-      <Contenedor titulo='Esto es un nuevo título.' nombre='Feo'>
+      {/* <Contenedor titulo='Esto es un nuevo título.' nombre='Feo'>
         <MasContenido />
         Todo es todo
       </Contenedor>
       <Contenido />
-      <Pie />
+      <Pie /> */}
+
+      {/* SOLUCIÓN -> Práctica 2.08 */}
+
+      <Cabecera />
+      <Contenedor>
+        <Peliculas />
+      </Contenedor>
     </React.Fragment>
   );
 }
