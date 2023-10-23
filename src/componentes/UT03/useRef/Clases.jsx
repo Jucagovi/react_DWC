5fc3b2be4a7d37bbda67ebabfa101110629b46e2
Jucagovi <jc.gomez@edu.gva.es>
2023-10-23 18:09:16 +0200

    Cambios clase

--- src/componentes/UT03/useRef/Clases.jsx
+++ src/componentes/UT03/useRef/Clases.jsx
@@ -10,6 +10,7 @@ const Clases = () => {
   // Funciones para los eventos de los botones.
   const ponerRojo = () => {
     contenedor1Ref.current.classList.toggle("rojo");
+    contenedor2Ref.current.classList.toggle("rojo");
   };
   const ponerAzul = () => {
     contenedor2Ref.current.classList.toggle("azul");
@@ -22,14 +23,14 @@ const Clases = () => {
         <button onClick={ponerAzul}>Clase azul</button>
       </div>
       <div>
-        {/* <BotonLimpiar
+        <BotonLimpiar
           referencia1={contenedor1Ref}
           referencia2={contenedor2Ref}
-        /> */}
+        />
       </div>
       <div>
-        <p ref={contenedor1Ref}>Este es el contenedor 1.</p>
-        <p ref={contenedor2Ref}>Este es el contenedor 2.</p>
+        <p ref={contenedor2Ref}>Este es el contenedor 1.</p>
+        <p ref={contenedor1Ref}>Este es el contenedor 2.</p>
       </div>
     </React.Fragment>
   );
