7bf3a22939ec77cf555936148b950df8cfccbce8
Jucagovi <jc.gomez@edu.gva.es>
2023-12-11 13:00:35 +0100

    UT05 asincronismo y hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,18 @@
 import React from "react";
 import "./App.css";
-import TresIntentos from "./componentes/UT04/TresIntentos.jsx";
-import EffectCiclo from "./componentes/UT04/useEffect/EffectCiclo.jsx";
-import EffectDOM from "./componentes/UT04/useEffect/EffectDOM.jsx";
-import EffectDependencias from "./componentes/UT04/useEffect/EffectDependencias.jsx";
-import EffectCronometro from "./componentes/UT04/useEffect/EffectCronometro.jsx";
+import Films from "./componentes/UT05/asincronismo/Films.jsx";
+import FilmsHook from "./componentes/UT05/asincronismo/FilmsHook.jsx";
+import FilmsHookManipular from "./componentes/UT05/asincronismo/FilmsHookManipular.jsx";
+import FilmsHookSituacion from "./componentes/UT05/asincronismo/FilmsHookSituacion.jsx";
 
 function App() {
   return (
     <React.Fragment>
-      <h1>Usando useEffect en React.</h1>
-      {/* Básico. */}
-      {/* <EffectCiclo /> */}
-      {/* Con dependencias. */}
-      {/* <EffectDependencias /> */}
-      {/* Accediendo al DOM y limpieza. */}
-      <EffectDOM />
+      <h1>Usando asincronismo en React.</h1>
+      <Films />
+      {/* <FilmsHook /> */}
+      {/* <FilmsHookManipular /> */}
+      {/* <FilmsHookSituacion /> */}
     </React.Fragment>
   );
 }
